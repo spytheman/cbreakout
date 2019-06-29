b5b8d8cbe445f52e338c32bf5e1dce0c3c224d87
Delyan Angelov <dido@bulsynt.org>
2019-06-30 02:07:10 +0300

    Misc.

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
+CC=g++
 CFLAGS=-g
 LDFLAGS=-lm
 OBJECTS=main.o graphics.o ball.o paddle.o brick.o
 
 all: cbreakout
 
-cbreakout: $(OBJECTS)
+main.o: main.cpp common.h
+graphics.o: graphics.cpp common.h
+paddle.o: paddle.cpp common.h
+ball.o: ball.cpp common.h
+brick.o: brick.cpp common.h
+
+cbreakout: $(OBJECTS) common.h
 	$(CC) $(CFLAGS) $(OBJECTS) -o cbreakout $(LDFLAGS)
 
 clean:
--- common.h
+++ common.h
@@ -1,4 +1,38 @@
 #ifndef __CBREAKOUT_COMMON_H
 #define __CBREAKOUT_COMMON_H
 
+class X {
+    public:
+    int x,y; // center of the object
+    int w,h; // half width and half height - distances to the edges
+};
+
+class Brick: public X {
+   Brick(int x, int y){
+      this->x = x;
+      this->y = y;
+      this->w = 40;
+      this->h = 10;
+   }
+};
+
+class Ball: public X{
+   Ball(int x, int y, int r){
+      this->x = x;
+      this->y = y;
+      this->w = r;
+      this->h = r;
+   }
+};
+
+class Paddle: public X{
+   Paddle(int x, int y){
+      this->x = x;
+      this->y = y;
+      this->w = 40;
+      this->h = 6;
+   }
+};
+
+
 #endif
