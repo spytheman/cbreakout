26e309d565ed06b9ec57980cb701ff52b5780d1e
Delyan Angelov <delian66@gmail.com>
2019-07-02 16:28:38 +0300

    Add 2 triangles instead of just one.

--- game.cpp
+++ game.cpp
@@ -7,7 +7,6 @@
 Game game;
 
 Game::Game(){
-   printf("Game()\n");
    this->init(); 
 }
 
@@ -18,28 +17,35 @@ void Game::init(){
 }
 
 void printGameState(Game *g){
-   printf("game->render() frames: %06d t:%4.3f |", g->frames, g->t);
+   printf("frames: %5d t:%8.3f |", g->frames, g->t);
    g->ball->print();
    g->paddle->print();
    g->bricks[0].print();
    printf("\n");
 }
 
-void showTriangle(){
+void showTriangle1(){
    glBegin(GL_TRIANGLES);
-   glColor3f(1.f, 0.f, 0.f);    glVertex3f(-0.6f, -0.4f, 0.f);
-   glColor3f(0.f, 1.f, 0.f);    glVertex3f( 0.6f, -0.4f, 0.f);
-   glColor3f(0.f, 0.f, 1.f);    glVertex3f( 0.f,   0.6f, 0.f);
+   glColor3f(1.f, 0.f, 0.f);    glVertex3f(-0.8f, -0.6f, 0.f);
+   glColor3f(0.f, 1.f, 0.f);    glVertex3f( 0.8f, -0.6f, 0.f);
+   glColor3f(0.f, 0.f, 1.f);    glVertex3f( 0.f,   0.8f, 0.f);
+   glEnd();
+}
+
+void showTriangle2(){
+   glBegin(GL_TRIANGLES);
+   glColor3f(0.5f, 0.f,  0.f);    glVertex3f(-0.5f, -0.4f, 0.3f);
+   glColor3f(0.f,  0.5f, 0.f);    glVertex3f( 0.5f, -0.4f, 0.3f);
+   glColor3f(0.f,  0.f,  0.5f);   glVertex3f( 0.f,   0.5f, 0.3f);
    glEnd();
 }
 
 void Game::render(){
    this->t = (float) glfwGetTime();
    this->frames++;
-   printGameState(this);
-   glRotatef( this->t * 50.f, 0.f, 0.f, 1.f);
-   showTriangle();
-//   usleep( 8000 + rand() % 1000 );
+   if( this->frames % 60 == 0 ) printGameState(this);
+   glRotatef( this->t * -50.f, 0.f, 0.f, 1.f);    showTriangle1();
+   glRotatef( this->t * 120.f, 2.f, 1.f, 1.f);    showTriangle2();
 }
 
 void Game::onKey(int key, int scancode, int action, int mods){
