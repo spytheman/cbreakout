832ee00bb88f773bae137fe6547c535801a9d29a
Delyan Angelov <delian66@gmail.com>
2019-07-03 18:02:41 +0300

    Extract showBackground/1 showPaddle/1 showBall/1 showBricks/1 .

--- game.cpp
+++ game.cpp
@@ -99,17 +99,46 @@ void showCube(){
    glEnd();   
 }
 
+void showPaddle(Game *g){
+   //glLoadIdentity(); glTranslatef( -0.5 * sin(this->t), -0.5, -0.5 ); glRotatef( this->t * 25.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  glutWireTeapot(1.0);
+   glLoadIdentity();
+   glTranslatef( 1.0 * g->paddle->x, 1.0 * g->paddle->y, 0);
+   glScalef( 1.0, 1.0, 1.0 );
+   glutWireCube(1.0);
+}
+void showBall(Game *g){
+   //glLoadIdentity(); glTranslatef( -0.5 * sin(this->t), -0.5, -0.5 ); glRotatef( this->t * 25.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  glutWireTeapot(1.0);
+   glLoadIdentity();
+   glTranslatef( 1.0 * g->ball->x, 1.0 * g->ball->y, 0);
+   glScalef( 1.0, 1.0, 1.0 );
+   glutWireSphere(1.0, 5, 5);
+}
+void showBricks(Game *g){
+   //glLoadIdentity(); glTranslatef( -0.5 * sin(this->t), -0.5, -0.5 ); glRotatef( this->t * 25.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  glutWireTeapot(1.0);
+   glLoadIdentity();
+   glTranslatef( 0.0, 0.0, 0.0);
+   glRotatef( 2.f, 0.f, 0.f, 1.5f);
+   glRotatef( g->t * -20.f, 0.f, 1.f, 0.0f);
+   glScalef( 0.2, 0.1, 0.1 );
+   glutWireCube(1.0);
+}
+void showBackground(Game *g){
+   glLoadIdentity(); glRotatef( g->t * -50.f, 0.f, 0.f, 1.f);    showTriangle1();
+   glLoadIdentity(); glRotatef( g->t * 120.f, 2.f, 1.f, 1.f);    showTriangle2();
+   glLoadIdentity(); glTranslatef(  0.0,  0.0,    0.5*cos(g->t) ); glRotatef( g->t *  70.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();
+   glLoadIdentity(); glTranslatef(  0.5,  0.5*cos(g->t),    0.5 ); glRotatef( g->t * -70.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();
+   glLoadIdentity(); glTranslatef( -0.5 * sin(g->t), -0.5, -0.5 ); glRotatef( g->t * 150.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();   
+   glLoadIdentity(); glTranslatef( -0.5 * sin(g->t), -0.5, -0.5 ); glRotatef( g->t * 25.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  glutWireTeapot(1.0);
+}
 void Game::render(){
    this->t = (float) glfwGetTime();
    this->frames++;
+   ///////////////////////////////////////////////////////
    if( this->frames % 60 == 0 ) printGameState(this);
-   glLoadIdentity(); glRotatef( this->t * -50.f, 0.f, 0.f, 1.f);    showTriangle1();
-   glLoadIdentity(); glRotatef( this->t * 120.f, 2.f, 1.f, 1.f);    showTriangle2();
-   glLoadIdentity(); glTranslatef(  0.0,  0.0,    0.5*cos(this->t) ); glRotatef( this->t *  70.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();
-   glLoadIdentity(); glTranslatef(  0.5,  0.5*cos(this->t),    0.5 ); glRotatef( this->t * -70.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();
-   glLoadIdentity(); glTranslatef( -0.5 * sin(this->t), -0.5, -0.5 ); glRotatef( this->t * 150.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  showCube();
-   
-   glLoadIdentity(); glTranslatef( -0.5 * sin(this->t), -0.5, -0.5 ); glRotatef( this->t * 25.f, 0.5f, 0.5f, 1.f);   glScalef( 0.5, 0.5, 0.5 );  glutWireTeapot(1.0);
+   showBackground(this);
+   showPaddle(this);
+   showBall(this);
+   showBricks(this);   
 }
 
 void Game::onKey(int key, int scancode, int action, int mods){
